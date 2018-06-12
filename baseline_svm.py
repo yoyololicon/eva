84884897bed6ee417e5901ded7b134041be9a95e
yoyololicon <ya70201@gmail.com>
2018-06-12 20:28:56 +0800

    testing different features, looks like spectral bandwidth is good

--- baseline_svm.py
+++ baseline_svm.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from sklearn.svm import SVC
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 from sklearn.model_selection import train_test_split, KFold
+from sklearn.utils import shuffle
 from sklearn.utils.class_weight import compute_class_weight, compute_sample_weight
 
 labels = pd.read_csv('annotations.csv')
@@ -27,6 +28,8 @@ y = np.nonzero(labels)[1]
 y_weights = dict(enumerate(compute_class_weight('balanced', np.unique(y), y)))
 X = LDA(n_components=2).fit_transform(features, y)
 
+figsize = (8, 6)
+plt.figure(figsize=figsize)
 colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'w']
 ax = []
 for c, i in zip(colors[:y.max() + 1], range(y.max() + 1)):
@@ -39,6 +42,7 @@ sy = np.nonzero(simple_labels)[1]
 sy_weights = dict(enumerate(compute_class_weight('balanced', np.unique(sy), sy)))
 sX = LDA(n_components=2).fit_transform(features, sy)
 
+plt.figure(figsize=figsize)
 ax = []
 for c, i in zip(colors[:sy.max() + 1], range(sy.max() + 1)):
     idx = np.where(sy == i)
@@ -46,27 +50,33 @@ for c, i in zip(colors[:sy.max() + 1], range(sy.max() + 1)):
 plt.legend(ax, [num2class[i + 2] for i in range(sy.max() + 1)])
 plt.show()
 
+feature_sets = {'mfcc': ['mfcc'],
+                'mfcc/contrast': ['mfcc', 'spectral_contrast'],
+                'mfcc/centroid': ['mfcc', 'spectral_centroid'],
+                'mfcc/bandwidth': ['mfcc', 'spectral_bandwidth'],
+                'mfcc/flatness': ['mfcc', 'spectral_flatness'],
+                'mfcc/rolloff': ['mfcc', 'spectral_rolloff'],
+                'mfcc/bandwidth/zcr': ['mfcc', 'spectral_bandwidth', 'zcr'],
+                'mfcc/bandwidth/rmse': ['mfcc', 'spectral_bandwidth', 'rmse'],
+                'all_features': list(features.columns.levels[0])}
+
+#features, y, sy = shuffle(features, y, sy)
+#print(features)
+
 clf = SVC(kernel='linear')
 kf = KFold(n_splits=3, shuffle=True)
-acc = []
-for train_idx, test_idx in kf.split(features, y):
-    X_train, X_test = features.loc[train_idx], features.loc[test_idx]
-    y_train, y_test = y[train_idx], y[test_idx]
 
-    clf.fit(X_train, y_train)
-    #weights = compute_sample_weight('balanced', y_test)
-    acc.append(clf.score(X_test, y_test))
+print("With original labels:")
+X_train, X_test, y_train, y_test, sy_train, sy_test = train_test_split(features, y, sy, test_size=0.3)
+for fset_name, fset in sorted(feature_sets.items(), key = lambda x : len(x[0])):
+    clf.fit(X_train.loc[:, fset], y_train)
+    weights = compute_sample_weight('balanced', y_test)
+    acc = clf.score(X_test.loc[:, fset], y_test, sample_weight=weights)
+    print(fset_name+":", acc)
 
-print("Average accuracy using linear svm:", np.mean(acc))
-
-acc = []
-for train_idx, test_idx in kf.split(features, sy):
-    X_train, X_test = features.loc[train_idx], features.loc[test_idx]
-    y_train, y_test = sy[train_idx], sy[test_idx]
-
-    clf.fit(X_train, y_train)
-    #weights = compute_sample_weight('balanced', y_test)
-    acc.append(clf.score(X_test, y_test))
-
-print("Average accuracy of simplified labels using linear svm:", np.mean(acc))
-exit()
+print("With simplified labels:")
+for fset_name, fset in sorted(feature_sets.items(), key = lambda x : len(x[0])):
+    clf.fit(X_train.loc[:, fset], sy_train)
+    weights = compute_sample_weight('balanced', sy_test)
+    acc = clf.score(X_test.loc[:, fset], sy_test, sample_weight=weights)
+    print(fset_name+":", acc)
